59f28ce4b93795780339e17b977032abf15300c9
Your Name <you@example.com>
2017-10-09 17:28:01 +0800

    Initial Commit

---
+++ GeneralProject/AppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.h
+//  GeneralProject
+//
+//  Created by Hanxiaojie on 2017/10/9.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GeneralProject/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  GeneralProject
+//
+//  Created by Hanxiaojie on 2017/10/9.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
